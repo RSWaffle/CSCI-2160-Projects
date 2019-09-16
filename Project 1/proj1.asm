9e00b4d9df9c0d9789c2ace09d4e5eb04377861f
Ryan <ryanthecat17@gmail.com>
2019-09-16 17:33:07 -0400

    update asm

--- Project 1/proj1.asm
+++ Project 1/proj1.asm
@@ -15,21 +15,58 @@
 	ExitProcess PROTO Near32 stdcall, dwExitCode:DWORD  ;executes "normal" termination
 	
 ;******************************************************************************************
-	.DATA				;declare all data identifiers after this directive
+	.DATA						;declare all data identifiers after this directive
 
 iResult DWORD  0 dup(?)			;memory to hold the resulting value of calculation
-sVal1 WORD 127			;sets the variable sVal1 to 127 decimal for calculation
-sVal2 WORD -25			;sets the variable sVal2 to -25 decimal for calculation
-iVal3 DWORD 78253		;sets the variable iVal3 to 78,253 decimal for calculation
-bVal BYTE 78		;sets the variable of BVal to 78, 200 decimal for calculation
+sVal1 WORD 127					;sets the variable sVal1 to 127 decimal for calculation
+sVal2 WORD -25					;sets the variable sVal2 to -25 decimal for calculation
+iVal3 DWORD 78253				;sets the variable iVal3 to 78,253 decimal for calculation
+bVal4 BYTE 78					;sets the variable of BVal to 78, 200 decimal for calculation
+
+sTemp WORD 1 dup(?)				;sets aside memory for a future value for calculation
+iTemp DWORD 0 dup(?)			;sets aside memory for a future value for calculation
 
 ;******************************************************************************************
 	.CODE
-_start:					;entry point for this program (needed for debugger)
+_start:							;entry point for this program (needed for debugger)
 
-	MOV EAX, 0			;aids in debugging and initalizes the program
+	MOV EAX, 0					;aids in debugging and initalizes the program
+	
+	MOV EBX, 0					;intitialize EBX to 0 to avoid calculation error
+	MOV AX ,sVal1				;move the first value of calculation to AX register
+	MOV BX, sVal2				;move the second valur of calculation to the BX register
+	ADD AX, BX					;add the two regiters and store in AX register	
+	MOV sTemp, AX				;store the value in AX into memory for later calculation
+	
+	MOV EBX, 0					;reset the EBX register to 0 to avoid calculation error
+	MOV EAX, iVal3				;move the first value of calculation to the EAX register
+	MOV BL,  bVal4				;move the second value of calculation to the EBX register makeing sure to match the byte size
+	SUB EAX, EBX				;preform the subtract calculation and store into the EAX register
+	MOV iTemp, EAX				;store the result in EAX to memory
+	
+	MOV ECX, 15					;set the counter to 15 so the loop knows when to terminate
+	MOV EAX, 0					;set the EAX register to 0 to avoid any calculation error
+	MOV BX, sTemp				;move the variable sTemp into a register so calculation can be done via register to register
+	
+lpMultiply1:					;loop for first multiplication operation
+	ADD AX, BX					;add the two values together and store into AX
+	loop lpMultiply1			;decrement the ECX register to eventually stop the loop, and jump to the top
+	
+	SUB AX, 20					;subtracts 20 from AX and stores back into AX
+	MOV sTemp, AX				;moves the result from the loop (stored in AX register) into the sTemp variable
+	
+	MOV CX, sTemp				;moves into the loop counter register the value of sTemp
+	MOV EAX, 0					;sets EAX register to 0 to avoid any calculation error
+	MOV EBX, iTemp				;set the value of EBX to iTemp for multiple additions to the same number
+	
+lpMultiply2:					;loop for the second multiplication operation
+	ADD EAX, EBX				;add the two registers together
+	loop lpMultiply2			;decrement the ECX register to eventually end the loop
+	
+	MOV iResult, EAX			;move the multiplication result into memeory as iResult
+
 	
 ;************************************* the instruction below calls for "normal termination"	
 	INVOKE ExitProcess,0
 	PUBLIC _start
-	END					;signals assembler that there are no instructions after this stmt
+	END							;signals assembler that there are no instructions after this stmt

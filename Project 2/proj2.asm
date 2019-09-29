8c186c3bb4f1d50a70188e1c385a3db78077d56c
RSWaffle <ryanthecat17@gmail.com>
2019-09-28 21:49:54 -0400

    Update proj2.asm

--- Project 2/proj2.asm
+++ Project 2/proj2.asm
@@ -13,14 +13,14 @@
 	.stack 100h					;EVERY program needs to have a stack allocated
 ;******************************************************************************************
 ;  List all necessary prototypes for methods to be called here
-	ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD  					;executes "normal" termination
-	intasc32 PROTO NEAR32 stdcall, lpStringToHold:dword, dval:dword			;will convert any D-Word number into ACSII characters
-	putstring  PROTO NEAR stdcall, lpStringToDisplay:dword  				;will display ;characters until the NULL character is found
-	getstring 	PROTO stdcall, lpStringToHoldInput:dword, maxNumChars:dword ;get input from user and convert. 
-	ascint32 PROTO NEAR32 stdcall, lpStringToConvert:dword  				;this converts ASCII characters to the dword value
+	ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD  					;Executes "normal" termination
+	intasc32 PROTO NEAR32 stdcall, lpStringToHold:dword, dval:dword			;Will convert any D-Word number into ACSII characters
+	putstring  PROTO NEAR stdcall, lpStringToDisplay:dword  				;Will display ;characters until the NULL character is found
+	getstring 	PROTO stdcall, lpStringToHoldInput:dword, maxNumChars:dword ;Get input from user and convert. 
+	ascint32 PROTO NEAR32 stdcall, lpStringToConvert:dword  				;This converts ASCII characters to the dword value
 	
 ;******************************************************************************************
-	.DATA						;declare all data identifiers after this directive
+	.DATA							;declare all data identifiers after this directive
 	
 	strEnterAmtNumbers byte 10,10, "How many values to input: ", 0
 	strEnterNumbers byte 10,10, "Type each value and press ENTER after each one: ", 0
@@ -31,15 +31,15 @@
 "       Class: CSCI 2160-001",10,
 "        Date: 10/04/2019",10,
 "         Lab: Project 2",0
-    crlf byte  10,13,0				;null-terminated string to skip to new line
-	strInput byte 60 dup (?)		;set aside 60 bytes of memory for strInput
+    crlf byte  10,13,0				;Null-terminated string to skip to new line
+	strInput byte 60 dup (?)		;Set aside 60 bytes of memory for strInput
 	sNumNumbers word 2				;Maximum number of chars that can be typed in the console for specifying how many numbers. 
 	sNum word 10					;Maximum number of chars that can be typed in the console for entering a number.
 	bOffset word 0					;This is going to hold the offset into the numbers variable.
-	iNumOfNums dword ?				;number of numbers to be input/calculated
-	iNumbers dword 10 dup (?)		;set aside 10 dwords in memory to hold future numbers.
-	iMaxNumber dword 4294967295		;maximum number for a dword for reference later
-	iTempNum dword ?				;temporary variable to be used for comparing later
+	iNumOfNums dword ?				;Number of numbers to be input/calculated
+	iNumbers dword 10 dup (?)		;Set aside 10 dwords in memory to hold future numbers.
+	iMaxNumber dword 4294967295		;Maximum number for a dword for reference later
+	iTempNum dword ?				;Temporary variable to be used for comparing later
 	
 	
 	
@@ -47,19 +47,19 @@
 	.CODE
 	
 _start:
-	MOV EAX, 0									;statement to help in debugging
+	MOV EAX, 0										;Statement to help in debugging
 	
 main PROC
 	
 getNumofNums:
-	INVOKE putstring, ADDR strProjInfo     			;skip to new line, tab, and display Project information "Name: Ryan Shupe" etc. 
-	INVOKE putstring, ADDR crlf						;display the characters to skip to a new line
-	INVOKE putstring, ADDR strEnterAmtNumbers  	    ;display the "Enter amount of numbers" message
+	INVOKE putstring, ADDR strProjInfo     			;Skip to new line, tab, and display Project information "Name: Ryan Shupe" etc. 
+	INVOKE putstring, ADDR crlf						;Display the characters to skip to a new line
+	INVOKE putstring, ADDR strEnterAmtNumbers  	    ;Display the "Enter amount of numbers" message
 	INVOKE getstring, ADDR strInput, sNumNumbers	;Take the string input and store it into a variable, max amount of chars typed is sNumChars
 	INVOKE ascint32, ADDR strInput					;Convert the ASCII value to its true decimal number
 	MOV iNumOfNums, EAX								;Move the result of above method stored in EAX into variable so it isnt lost.
-	MOV ECX, iNumOfNums								;put the value of iNumOfNums into ECX so we can use it to loop later
-	MOV EDI, 0										;put 0 into EDI so we can start at a 0 offset into iNumbers
+	MOV ECX, iNumOfNums								;Put the value of iNumOfNums into ECX so we can use it to loop later
+	MOV EDI, 0										;Put 0 into EDI so we can start at a 0 offset into iNumbers
 		
 	CMP iNumOfNums, 0								;Compare iNumOfNums to 0 to see if the user typed null character
 	JE maxAmountMessage								;If it is null then jump to maxAmountMessage
@@ -70,10 +70,10 @@ getNumofNums:
 
 getNums:
 	
-	INVOKE putstring, ADDR strEnterNumbers   		;display the "Type each value and press ENTER after each one:" message
+	INVOKE putstring, ADDR strEnterNumbers   		;Display the "Type each value and press ENTER after each one:" message
 	lpGetNums:
-		MOV EAX, 0									;reset EAX to 0 to prevent errors
-		INVOKE putstring, ADDR crlf					;display the characters to skip to a new line
+		MOV EAX, 0									;Reset EAX to 0 to prevent errors
+		INVOKE putstring, ADDR crlf					;Display the characters to skip to a new line
  
 		INVOKE getstring, ADDR strInput, sNum		;Take the string input and store it into a variable, max amount of chars typed is sNumChars
 		INVOKE ascint32, ADDR strInput				;Convert the ASCII value to its true decimal number
@@ -83,40 +83,40 @@ getNums:
 		CMP iTempNum, 0								;Compare EAX to 0 to see if the user typed null character
 		JE invalidNum							    ;If it is null then jump to maxAmountMessage
 		CMP EDX, iTempNum							;Compare EDX to maximum dWord value
-		JG invalidNum								;if it is greater than, jump to invalidNum section
+		JG invalidNum								;If it is greater than, jump to invalidNum section
 		CMP EDX, iTempNum							;Compare EDX to maximum dWord value
-		JLE validNum								;if the number complies, jump to the validNum section
+		JLE validNum								;If the number complies, jump to the validNum section
 
 		invalidNum:
-			INVOKE putstring, ADDR crlf				;display the characters to skip to a new line
-			INVOKE putstring, ADDR strMaxNumber		;display the max possible dword value string
-			JMP getNums								;jump to the top and repeat until complied
+			INVOKE putstring, ADDR crlf				;Display the characters to skip to a new line
+			INVOKE putstring, ADDR strMaxNumber		;Display the max possible dword value string
+			JMP getNums								;Jump to the top and repeat until complied
 	
 		validNum:
-			MOV iNumbers[EDI], EAX					;move EBX into iNumbers to be saved for later
+			MOV iNumbers[EDI], EAX					;Move EBX into iNumbers to be saved for later
 			ADD EDI, 4								;Add 4 to EDI to put the number into the correct place in iNumbers
 			
-	loop lpgetNums									;keep looping this until all of the numbers to be entered are filled.
+	loop lpgetNums									;Keep looping this until all of the numbers to be entered are filled.
 	
 JMP calculation										;Jump to the calculation section to preform the required calculation
 	
 maxAmountMessage:
-	MOV ECX, 100									;set ECX to 100 to let the loop know when to terminate and how many lines to skip
-	lpClearSc:										;loop to simulate a clear screen
-		INVOKE putstring, ADDR crlf					;display the characters to skip to a new line
-		loop lpClearSc								;decrement ECX so the loop knows when to terminate
-	INVOKE putstring, ADDR strMaxAmount				;display a message letting the user know that the maximum amount of numbers to enter is 10
-	JMP getNumofNums								;jump back up to the getNumofNums section and it will repeat until the user enters a value less than or equal to 10
+	MOV ECX, 100									;Set ECX to 100 to let the loop know when to terminate and how many lines to skip
+	lpClearSc:										;Loop to simulate a clear screen
+		INVOKE putstring, ADDR crlf					;Display the characters to skip to a new line
+		loop lpClearSc								;Decrement ECX so the loop knows when to terminate
+	INVOKE putstring, ADDR strMaxAmount				;Display a message letting the user know that the maximum amount of numbers to enter is 10
+	JMP getNumofNums								;Jump back up to the getNumofNums section and it will repeat until the user enters a value less than or equal to 10
 	
 	calculation:
 	jmp finished
 	
 	
-;************************************* the instruction below calls for "normal termination"	
+;************************************* the instructions below calls for "normal termination"	
 finished:
 	INVOKE ExitProcess,0						 
 	PUBLIC _start
 	
 main ENDP
-	END												;signals assembler that there are no instructions after this statement
+	END												;Signals assembler that there are no instructions after this statement
 	

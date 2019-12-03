3bacf11f0fa008e946f58cbed4d8633f2f150c4f
Ryan <ryanthecat17@gmail.com>
2019-12-02 23:04:22 -0500

    help

--- Project 6/proj6a.asm
+++ Project 6/proj6a.asm
@@ -15,9 +15,6 @@
 ;******************************************************************************************
 .DATA
 x dword 5
-dValArray dword 10 dup (0)
-xTemp dword 0
-loopCheck byte 1
 ;******************************************************************************************
 .CODE
 
@@ -52,53 +49,6 @@ fun PROC stdcall
 	MOV EBP, ESP						;set a new stack frame
 	
 	@n EQU DWORD PTR [EBP + 8]			;point @n to ebp + 8 so we don't have to reference it all the time
-
-	MOV EBX, @n							;moves the n value into ebx
-	MOV xTemp, EBX
-	DEC EBX								;decrement it to get n-1
-	PUSH EBX							;push to the stack for calling
-	CALL funplus							;call the function again
-	ADD ESP, 4							;add back the number of bytes we used
-	ADD dValArray, EAX
-
-	
-	MOV EBX, xTemp							;moves the n value into ebx
-	DEC EBX
-	DEC EBX
-	PUSH EBX							;push to the stack for calling
-	CALL funplus						;call the function again
-	ADD ESP, 4							;add back the number of bytes we used
-	POP EBX
-	
-	ADD EAX, EAX
-	ADD dValArray, EAX
-	
-
-	Done:
-	POP EBX
-	POP EBP								;restore the original EBP
-	RET 								;return 
-	
-fun ENDP
-
-COMMENT%
-
-******************************************************************************
-*Name: funplus	                                                             *
-*Purpose:                                                                    *
-*	  A recursive equation for fun(n-1) + 2*fun(n-2)+3*fun(n-3), for n>2     *
-*		 					possible array to track the results	     		 *
-*Date Created: 11/26/2019                                                    *
-*Date Modified: 11/26/2019                                                   *
-*                                                                            *
-*                                                                            *
-*@param inVal:dword                                                          *
-*****************************************************************************%
-funplus PROC stdcall
-	PUSH EBP 							;preserve the old stack frame
-	MOV EBP, ESP						;set a new stack frame
-	
-	@n EQU DWORD PTR [EBP + 8]			;point @n to ebp + 8 so we don't have to reference it all the time
 	PUSH @n								;preserve the value @n points to
 	
 	.IF @n == 1							;if @n is equal to 1
@@ -111,19 +61,21 @@ funplus PROC stdcall
 		MOV EAX, 7						;move 7 into the output	
 		JMP Done						;jump to the done section
 	.ENDIF								;end if
-
-	MOV EBX, @n							;moves the n value into ebx
-	DEC EBX								;decrement it to get n-1
-	PUSH EBX							;push to the stack for calling
-	CALL fun							;call the function again
-	ADD ESP, 4							;add back the number of bytes we used
-	Done:
-	POP EBX
-	POP EBP								;restore the original EBP
-	RET 								;return 
 	
-funplus ENDP
+	MOV EBX, @n
+	DEC EBX
+	PUSH EBX
+	CALL fun
+	ADD ESP, 4
+	ADD EAX, @n
+ 
 
+
+Done:
+	POP EBX
+	POP EBP
+	RET
+fun ENDP
 	
 ;************************************* the instructions below calls for "normal termination"	
 INVOKE ExitProcess,0						 

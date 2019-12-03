72042d4efed3cffce6f0f3f4891f5d0bf112e21d
Ryan <ryanthecat17@gmail.com>
2019-12-03 02:09:20 -0500

    recursion progress

--- Project 6/proj6a.asm
+++ Project 6/proj6a.asm
@@ -14,7 +14,9 @@
 	ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD  						;Executes "normal" termination
 ;******************************************************************************************
 .DATA
-x dword 5
+x dword 4
+dVal dword ?
+temp dword ?
 ;******************************************************************************************
 .CODE
 
@@ -22,11 +24,14 @@ _start:
 
 	MOV EAX, 0
 	MOV EDX, 0
+	XOR ECX, ECX
 main PROC
 
 	PUSH x
+	MOV temp, ESP
 	call fun
 	ADD esp, 4
+	JMP finish
 main ENDP
 
 
@@ -36,7 +41,7 @@ COMMENT%
 ******************************************************************************
 *Name: fun	                                                                 *
 *Purpose:                                                                    *
-*	  A recursive equation for fun(n-1) + 2*fun(n-2)+3*fun(n-3), for n>2     *
+*	  A recursive equation for fun(n-1) + 2(fun(n-2))+ 3(fun(n-3)), for n>2     *
 *		 					possible array to track the results	     		 *
 *Date Created: 11/26/2019                                                    *
 *Date Modified: 11/26/2019                                                   *
@@ -47,36 +52,91 @@ COMMENT%
 fun PROC stdcall
 	PUSH EBP 							;preserve the old stack frame
 	MOV EBP, ESP						;set a new stack frame
+	MOV temp, EBP
 	
 	@n EQU DWORD PTR [EBP + 8]			;point @n to ebp + 8 so we don't have to reference it all the time
-	PUSH @n								;preserve the value @n points to
+
 	
 	.IF @n == 1							;if @n is equal to 1
 		MOV EAX, 2						;move 2 into the output
+
 		JMP Done						;jump to done section
 	.ELSEIF @n == 0						;if @n is equal to 0
 		MOV EAX, 1						;move 1 into the output
+
 		JMP Done						;jump to done
 	.ELSEIF @n == 2						;if @n is equal to 2
 		MOV EAX, 7						;move 7 into the output	
+
 		JMP Done						;jump to the done section
 	.ENDIF								;end if
-	
+
 	MOV EBX, @n
+	DEC EBX
+	PUSH EBX
+	INC ECX
+	CALL fun
+	ADD ESP, 4
+	
+	.WHILE	ECX != 1
+		DEC ECX
+		JMP Done
+		
+	.ENDW
+	
+	.IF ECX == 1 && EDX == 1
+	
+		ADD EAX, EAX
+		JMP Done
+		
+	.ENDIF
+	
+	.IF ECX == 1 && EDX == 2
+	
+		MOV EDX, EAX
+		ADD EAX, EAX
+		ADD EAX, EDX
+		
+		JMP Done
+		
+	.ENDIF
+	
+	ADD dVal, EAX
+	
+	MOV ECX, 0
+	MOV EDX, 1
+	MOV EBX, @n
+	DEC EBX
 	DEC EBX
 	PUSH EBX
 	CALL fun
 	ADD ESP, 4
-	ADD EAX, @n
- 
 
+	ADD dVal, EAX
+	
+	MOV ECX, 0
 
+	MOV EBX, @n
+	DEC EBX
+	DEC EBX
+	DEC EBX
+	PUSH EBX
+	CALL fun
+	ADD ESP, 4
+	
+	MOV EDX, EAX
+	ADD EAX, EAX
+	ADD EAX, EDX
+	
+	ADD dVal, EAX
+	
 Done:
-	POP EBX
+	
 	POP EBP
 	RET
 fun ENDP
 	
+finish:
 ;************************************* the instructions below calls for "normal termination"	
 INVOKE ExitProcess,0						 
 PUBLIC _start
